17038f4497d190eab8e2044b3125331f8c90e198
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-10-02 10:41:13 -0400

    update chatbot

--- RobotProject_JaneFeng/src/chatbotProject/Chatbot.java
+++ RobotProject_JaneFeng/src/chatbotProject/Chatbot.java
@@ -4,10 +4,16 @@ package chatbotProject;
 public class Chatbot {
 		private String userName;
 		private Topic jane;
+		private Topic lubna;
+		private Topic mimi;
+		private Topic jessi;
 		private boolean chatting;
 
 	public Chatbot() {
 		jane= new ChatbotJane();
+		lubna= new ChatbotLubna();
+		mimi= new ChatbotMimi();
+		jessi= new ChatbotJessi();
 		userName="unknown user";
 	}		
 	public void startTalkin() {
@@ -17,9 +23,21 @@ public class Chatbot {
 		while(chatting) {
 			ChatbotMain.print("What do you want to talk about?");
 			String response = ChatbotMain.getInput();
-			if(jane.isTriggered(response)) {
+			if(lubna.isTriggered(response)) {
+				chatting = false;
+				lubna.startChatting(response);
+			}
+			else if(mimi.isTriggered(response)) {
+				chatting = false;
+				mimi.startChatting(response);
+			}
+			else if(jane.isTriggered(response)) {
 				chatting = false;
 				jane.startChatting(response);
+			}
+			else if(jessi.isTriggered(response)) {
+				chatting = false;
+				jessi.startChatting(response);
 			}
 			else
 				ChatbotMain.print("I'm sorry. I don't understand");
