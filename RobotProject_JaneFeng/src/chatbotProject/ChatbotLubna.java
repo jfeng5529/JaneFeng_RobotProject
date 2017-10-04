8d68a01dda96bcf8bbf1417b0aad8aadff99c2e8
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-10-04 11:43:45 -0400

    lubna didn't do anything today

--- RobotProject_JaneFeng/src/chatbotProject/ChatbotLubna.java
+++ RobotProject_JaneFeng/src/chatbotProject/ChatbotLubna.java
@@ -8,6 +8,7 @@ public class ChatbotLubna implements Topic {
 	private String  secretWord;
 	private boolean chatting;
 	private ChatbotJane jane;
+	private String[] keywords;
 
 	public ChatbotLubna() {
 		String[] temp = {"ugly","dumb","mean", "stupid","dull","foolish"};
@@ -15,12 +16,20 @@ public class ChatbotLubna implements Topic {
 		String[] temp2 = {"lovely","nice","sweet","pretty","kind","great","like","love","beautiful","funny"};
 		lovewords = temp2;
 		secretWord = "potato";
+		String[] temp3 = {"food","dates","interests", "like","jokes","magic","stars","crystal ball"};
+		keywords = temp3;
+		//magic=jessi, stars= mimi,jane= crystal ball;
 	}
 
 	
 	public boolean isTriggered(String response) {
 		jane = ChatbotMain.chatbot.getJane();
-		for (int i = 0; i<meanwords.length; i++) {
+		for(int i = 0; i<keywords.length; i++) {
+			if)(ChatbotMain.findKeyword(response, keywords[i], 0) >=0){
+				return true;
+			}
+		}
+		/*for (int i = 0; i<meanwords.length; i++) {
 			if(ChatbotMain.findKeyword(response, meanwords[i], 0) >= 0) {
 				jane.decreaseLoveCount();
 				return true;
@@ -30,14 +39,17 @@ public class ChatbotLubna implements Topic {
 				if(ChatbotMain.findKeyword(response, lovewords[i], 0) >= 0) {
 					jane.increaseLoveCount();
 					return true;
-		}
+		}*/
 			}
 			return false;
 		
 	}
-
 	public void startChatting(String response) {
-		ChatbotMain.print("Hey! You could be a little nicer.");
+		ChatbotMain.print();
+	}
+
+	/*public void startChatting(String response) {
+		ChatbotMain.print("Aww you so sweet!");
 		chatting = true;
 		while(chatting) {
 			 response = ChatbotMain.getInput();
@@ -52,7 +64,7 @@ public class ChatbotLubna implements Topic {
 				ChatbotMain.print("Huh. I don't really get you.");
 			}
 		}
-	}
+	}*/
 	public void practiceLove() {
 		int loveCount = ChatbotMain.chatbot.getJane().getLoveCount();
 	}
